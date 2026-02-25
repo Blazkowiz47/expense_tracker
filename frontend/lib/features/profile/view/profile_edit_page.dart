109a128ef2299fe01bb9e73c2c056715ad82bbd7
sushrutpatwardhan@gmail.com <sushrutpatwardhan@Sushruts-MacBook-Pro.local>
2026-02-25 15:22:27 +0100

    frontend: clean profile avatar cache-refresh imports

--- frontend/lib/features/profile/view/profile_edit_page.dart
+++ frontend/lib/features/profile/view/profile_edit_page.dart
@@ -6,7 +6,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileEditPage extends StatefulWidget {
